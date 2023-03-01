53a09d18d0cea237436828f2dbd26c99ebd85c9d
NTsch <niklas.tscherne@proton.me>
2023-03-01 12:17:16 +0100

    introduced functions for getting all collection info and downloading specific pages

--- main.py
+++ main.py
@@ -11,52 +11,83 @@ session_id = response.headers['Set-Cookie'].split(';')[0]
 # Set up the headers for subsequent requests
 headers = {'Cookie': session_id}
 
-# Get a list of collections
-collections_url = 'https://transkribus.eu/TrpServer/rest/collections/list'
-response = requests.get(collections_url, headers=headers)
-collections = response.json()
-
 # Create a directory to save the documents
 if not os.path.exists('documents'):
     os.mkdir('documents')
 
-# Save the list of collections
-collections_file = 'documents/collections.json'
-with open(collections_file, 'w', encoding='utf-8') as f:
-    json.dump(response.json(), f, indent=4)
+def get_everything():
+    # Get a list of collections
+    collections_url = 'https://transkribus.eu/TrpServer/rest/collections/list'
+    response = requests.get(collections_url, headers=headers)
+    collections = response.json()
 
-# Loop over each collection
-for collection in collections:
-    collection_id = collection['colId']
-    collection_name = collection['colName']
-    print(f'Processing collection {collection_name}')
-
-    # Create a subdirectory for the collection
-    collection_dir = os.path.join('documents', collection_name)
-    if not os.path.exists(collection_dir):
-        os.mkdir(collection_dir)
-
-    # Get a list of documents for the collection and save it
-    documents_url = f'https://transkribus.eu/TrpServer/rest/collections/{collection_id}/list'
-    response = requests.get(documents_url, headers=headers)
-    documents = response.json()
-    
-    documents_file = os.path.join(collection_dir, f'{collection_name}_collection.json')
-    with open(documents_file, 'w', encoding='utf-8') as f:
+    # Save the list of collections
+    collections_file = 'documents/collections.json'
+    with open(collections_file, 'w', encoding='utf-8') as f:
         json.dump(response.json(), f, indent=4)
 
-    # Loop over each document in the collection
-    for document in documents:
-        document_id = document['docId']
-        document_name = document['title']
+    # Loop over each collection
+    for collection in collections:
+        collection_id = collection['colId']
+        collection_name = collection['colName']
+        print(f'Processing collection {collection_name}')
 
-        # Retrieve the full document
-        document_url = f'https://transkribus.eu/TrpServer/rest/collections/{collection_id}/{document_id}/fulldoc'
-        response = requests.get(document_url, headers=headers)
+        # Create a subdirectory for the collection
+        collection_dir = os.path.join('documents', collection_name)
+        if not os.path.exists(collection_dir):
+            os.mkdir(collection_dir)
 
-        # Save the document to a file
-        document_file = os.path.join(collection_dir, f'{document_name}.json')
-        with open(document_file, 'w', encoding='utf-8') as f:
+        # Get a list of documents for the collection and save it
+        documents_url = f'https://transkribus.eu/TrpServer/rest/collections/{collection_id}/list'
+        response = requests.get(documents_url, headers=headers)
+        documents = response.json()
+    
+        documents_file = os.path.join(collection_dir, f'{collection_name}_collection.json')
+        with open(documents_file, 'w', encoding='utf-8') as f:
             json.dump(response.json(), f, indent=4)
 
-print('All documents saved')
+        # Loop over each document in the collection
+        for document in documents:
+            document_id = document['docId']
+            document_name = document['title']
+
+            # Retrieve the full document
+            document_url = f'https://transkribus.eu/TrpServer/rest/collections/{collection_id}/{document_id}/fulldoc'
+            response = requests.get(document_url, headers=headers)
+
+            # Save the document to a file
+            document_file = os.path.join(collection_dir, f'{document_name}.json')
+            with open(document_file, 'w', encoding='utf-8') as f:
+                json.dump(response.json(), f, indent=4)
+
+    print('All documents saved')
+
+def get_xml(document_id, page_no, collection_id=87230):
+
+    # Get the desired document as a JSON file
+    doc_url = f'https://transkribus.eu/TrpServer/rest/collections/{collection_id}/{document_id}/fulldoc'
+    response = requests.get(doc_url, headers=headers)
+    doc_json = response.json()
+    urls = []
+
+    # Find the URLs for the PAGE-XML transcription files of the given page
+    for page in doc_json['pageList']['pages']:
+        if page['pageNr'] == page_no:
+            for transcript in page['tsList']['transcripts']:
+                urls.append(transcript['url'])
+    
+    # Download and save all XMLs
+    for url in urls:
+        response = requests.get(url)
+        if response.status_code == 200:
+            content = response.content
+        
+            with open(url.split('=')[1] + '.xml', 'wb') as f:
+                f.write(content)
+            
+            print(f"File {url.split('=')[1]}.xml saved successfully.")
+        else:
+            print(f"Error downloading file from URL {url}.")
+        
+
+get_xml(document_id=1002019, page_no=1)
